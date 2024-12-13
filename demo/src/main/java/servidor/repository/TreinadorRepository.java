9783a0dec07a4a5f4823f57cce7134ccafa11577
Bruno <145134344+nephbt@users.noreply.github.com>
2024-12-13 11:15:14 -0300

    Delete demo directory

--- demo/src/main/java/servidor/repository/TreinadorRepository.java
+++
@@ -1,9 +0,0 @@
-package servidor.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import servidor.model.Treinador;
-import java.util.List;
-
-public interface TreinadorRepository extends JpaRepository<Treinador, Long> {
-    List<Treinador> findByNome(String nome);
-}
